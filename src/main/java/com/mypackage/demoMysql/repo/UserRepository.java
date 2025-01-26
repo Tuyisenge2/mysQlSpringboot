a1b06bc2962f592b5c22077166b708b3716bbeb6
Tuyisenge2 <tuyisengetito3@gmail.com>
2025-01-26 13:30:16 +0200

    first commit

---
+++ src/main/java/com/mypackage/demoMysql/repo/UserRepository.java
@@ -0,0 +1,16 @@
+package com.mypackage.demoMysql.repo;
+
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mypackage.demoMysql.model.User;
+
+
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	Optional<User> findByUsernameOrEmail(String username, String email);
+}
+
+
